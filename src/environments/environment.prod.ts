93b93a4190be213274ca9fc49ce4982d26f0c2a0
Andrei Rînea <arinea@intralinks.com>
2022-04-18 20:00:04 +0300

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA-KuMgBTbNFDbcEun1Ezobv3m3Ihv0_n0",
+    authDomain: "first-project-455b0.firebaseapp.com",
+    projectId: "first-project-455b0",
+    storageBucket: "first-project-455b0.appspot.com",
+    messagingSenderId: "105182156770",
+    appId: "1:105182156770:web:5db76433b0de82c4810bbb",
+    measurementId: "G-FE31DPJJ5N"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA-KuMgBTbNFDbcEun1Ezobv3m3Ihv0_n0",
+    authDomain: "first-project-455b0.firebaseapp.com",
+    projectId: "first-project-455b0",
+    storageBucket: "first-project-455b0.appspot.com",
+    messagingSenderId: "105182156770",
+    appId: "1:105182156770:web:5db76433b0de82c4810bbb",
+    measurementId: "G-FE31DPJJ5N"
+  }
 };
 
 /*
