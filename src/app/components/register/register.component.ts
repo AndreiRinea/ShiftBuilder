2130775f08d85a42e77f942eab1a9746505378e5
Andrei Rînea <arinea@intralinks.com>
2022-04-18 20:57:28 +0300

    Added first contents of Register component: a username field and a button for registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get username() {
+    let usernameControl = this.registerForm.get("username") as FormControl;
+    return usernameControl.value;
+  }
+
+  register() {
+    console.log(this.username);
+    console.log(this.registerForm.invalid);
+  }
 }
